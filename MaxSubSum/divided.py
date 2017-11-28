21a92063396cc0366ba3a3c1bd3ac919044f46bd
JachinYin <1106025235@qq.com>
2017-11-28 20:22:27 +0800

    我也不知道

--- MaxSubSum/divided.py
+++ MaxSubSum/divided.py
@@ -8,7 +8,7 @@ from randomNumber import *
 
 pro = 0
 las = 0
-def maxSunSum(left,right):
+def maxSubSum(left, right):
     global pro,las
     sum = 0
     if left == right:
@@ -18,8 +18,8 @@ def maxSunSum(left,right):
             sum = 0
     else:
         center = (left + right)//2
-        leftSum = maxSunSum(left,center)        #情况1
-        rightSum = maxSunSum(center+1,right)    #情况2
+        leftSum = maxSubSum(left, center)        #情况1
+        rightSum = maxSubSum(center + 1, right)    #情况2
         #情况3
         s1 = 0
         lefts = 0
@@ -33,11 +33,14 @@ def maxSunSum(left,right):
             rights += test_arr[j]
             if rights > s2:
                 s2 = rights
-        sum = s1+s2
-        if sum < leftSum:
-            sum = leftSum
-        if sum < rightSum:
-            sum = rightSum
+
+        #返回左边，右边，跨中间 的最大的一个值
+        sum = max(s1+s2,leftSum,rightSum)
+
+        # if sum < leftSum:
+        #     sum = leftSum
+        # if sum < rightSum:
+        #     sum = rightSum
 
         # if sum < leftSum :#and leftSum > rightSum:
         #     sum = leftSum
@@ -56,9 +59,10 @@ def maxSunSum(left,right):
     return sum
 
 begin_time =  datetime.datetime.now()
-print '最大子段和:' + str(maxSunSum(0,len(test_arr)-1))
+print '最大子段和:' + str(maxSubSum(0, len(test_arr) - 1))
 end_time =  datetime.datetime.now()
 
 #print '从第 %d 个数到 第 %d 个数'%(pro,las)
 
 print '算法用时%sS' %str((end_time-begin_time).total_seconds())
+

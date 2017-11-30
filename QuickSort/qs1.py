de8bf6247c5f593bd46e77aaeeee3e0753512334
JachinYin <1106025235@qq.com>
2017-11-30 10:53:25 +0800

    快速排序

---
+++ QuickSort/qs1.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+'''
+快速排序
+三路排序
+Author: Jachin
+Data: 2017- 11- 30
+'''
+from randomNumber import * #引入随机数列表生成模块
+import datetime             #引入该模块统计算法运行时间
+
+def quickSort(arr):
+    '''
+    快速排序算法1
+    :param arr:实数的列表
+    :return: 排序后的列表
+    '''
+    less = []
+    pivotList = []
+    more = []
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        for i in arr:
+            if i < pivot:
+                less.append(i)
+            elif i > pivot:
+                more.append(i)
+            else:
+                pivotList.append(i)
+
+        less = quickSort(less)
+        more = quickSort(more)
+
+        return less + pivotList + more
+
+begin_time =  datetime.datetime.now()
+quickSort(number_arr)
+end_time =  datetime.datetime.now()
+
+print '算法用时 %sS' %str((end_time-begin_time).total_seconds())
---
+++ QuickSort/randomNumber.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+产生随机数组
+Author: Jachin
+Data: 2017- 11- 27
+'''
+
+
+import random
+
+LENGTH = 100000 #生成随机数个数
+BEGIN , END= -1000,1000 #生成随机数的范围取值
+
+number_arr = [random.randint(BEGIN, END) for i in range(LENGTH)]

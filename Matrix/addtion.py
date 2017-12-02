cc4c4397539351738ac08574c474bc0aaecf7616
JachinYin <1106025235@qq.com>
2017-12-02 21:17:51 +0800

    添加分目

---
+++ Matrix/addtion.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+'''
+Data: 2017- 12 - 02
+Author: Jachin
+This is a scrpit for ...
+'''
+
+import numpy as np
+
+def Addition(matr1,matr2):
+    if matr1 and matr2:
+        if len(matr1)==len(matr1) and len(matr1[0])==len(matr2[0]):
+            c = []
+            for row in range(len(matr1)):
+                r = []
+                for column in range(len(matr1[0])):
+                    r.append(matr1[row][column] + matr2[row][column])
+                c.append(r)
+            for i in  range(len(c)):
+                print c[i]
+        else:
+            print "请输入行列一致的矩阵"
+    else:
+        print "请输入非空矩阵"
+
+def Multipication(matr1,matr2):
+    return np.multiply(matr1,matr2)
+
+m1 = [[1,2],
+      [2,5]
+      ]
+m2 = [[2,3],
+      [4,9]
+      ]
+
+Addition(m1,m2)
+print Multipication(m1,m2)
+
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Python-Algorithm
 
 一些经典算法的python实现
+
+1. 快速排序
+2. 最长子段和问题
+3. 最长公共子序列LCS

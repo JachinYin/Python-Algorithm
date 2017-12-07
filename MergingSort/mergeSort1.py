bf19feb8a74dd8a559a7486ee1d871641c0defe6
JachinYin <1106025235@qq.com>
2017-12-07 10:17:32 +0800

    归并排序算法1

---
+++ MergingSort/mergeSort1.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+'''
+Data: 2017- 12 - 07
+Author: Jachin
+归并排序
+'''
+
+def megeSort(arr):
+    if len(arr) == 1:
+        return arr
+    mid = len(arr)/2
+    left = megeSort(arr[:mid])
+    right = megeSort(arr[mid:])
+
+    i = 0  # left 计数
+    j = 0  # right 计数
+    k = 0  # 总计数
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+    remain = left if i < j else right
+    r = i if remain == left else j
+
+    while r < len(remain):
+        arr[k] = remain[r]
+        r += 1
+        k += 1
+
+    return arr
+
+test_arr = [1,4,3,2,7,9]
+print megeSort(test_arr)

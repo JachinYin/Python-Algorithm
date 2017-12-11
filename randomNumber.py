f1c54517b9084631a5d3359b9f8cb6f2ebe69dac
JachinYin <1106025235@qq.com>
2017-12-11 16:04:46 +0800

    生成随机数列
    使用函数写法，方便自定义生成大小，以及范围

--- randomNumber.py
+++ randomNumber.py
@@ -8,7 +8,17 @@ Data: 2017- 11- 27
 import datetime
 import random
 
-LENGTH = 1000 #生成随机数个数
-BEGIN , END= -1000,1000 #生成随机数的范围取值
+number_arr = []
+def getArray(LENGTH = 10000,BEGIN = -1000, END = 1000):
+    '''
+    生成随机数列
+    :param LENGTH: 生成随机数个数
+    :param BEGIN: 生成的随机数的最小值
+    :param END:  生成的随机数的最大值
+    :return:
+    '''
+    global number_arr
+    number_arr = [random.randint(BEGIN, END) for i in range(LENGTH)]
+    return number_arr
 
-number_arr = [random.randint(BEGIN, END) for i in range(LENGTH)]
+getArray()

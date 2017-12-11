cd485049ff447025a626ea1845eb47c617046a99
JachinYin <1106025235@qq.com>
2017-12-11 16:07:49 +0800

    希尔排序

---
+++ ShellSort/shellSort.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+'''
+Data: 2017- 12 - 11
+Author: Jachin
+This is a scrpit for Shell's sort
+'''
+
+def shellSort1(arr):
+    gap = len(arr)/2
+    if gap == 1:
+        return arr
+
+
+def shellsort3(arr):
+    n = len(arr)
+    gap = n / 2
+    while gap > 0:
+        gap = gap / 2
+        for j in range(gap,n):
+            if (arr[j] < arr[j - gap]):# // 每个元素与自己组内的数据进行直接插入排序
+                temp = arr[j]
+                k = j - gap
+                while k >= 0 and arr[k] > temp:
+
+                    arr[k + gap] = arr[k]
+                    k -= gap
+                    arr[k + gap] = temp
+    return arr
+
+A = [49,38,65,97,26,13,27,49,55,4]
+
+print shellsort3(A)

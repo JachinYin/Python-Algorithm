f4a3bfc60dbef0512e01e1bce05f2d85ab6a60bb
JachinYin <1106025235@qq.com>
2017-12-17 13:20:15 +0800

    最大子段和 动态规划法

--- MaxSubSum/dynamic.py
+++ MaxSubSum/dynamic.py
@@ -10,7 +10,7 @@ from randomNumber import *
 pro = 0
 las = 0
 
-def maxSubSum(s):
+def maxSubSum3(s):
     global pro,las
     thisSum = 0
     maxSum = 0
@@ -27,10 +27,11 @@ def maxSubSum(s):
 
 
 begin_time =  datetime.datetime.now()
-print '最大子段和:' + str(maxSubSum(number_arr))
+print '最大子段和:' + str(maxSubSum3(number_arr))
 end_time =  datetime.datetime.now()
 
 print '从第 %d 个数到 第 %d 个数'%(pro+1,las+1)
 
-print '算法用时%sS' %str((end_time-begin_time).total_seconds())
+time3 = (end_time-begin_time).total_seconds()
+print '用动态规划排列%d个数用时%sS' %(len(number_arr),str(time3))
 

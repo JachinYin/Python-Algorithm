4feaf8f6b437e6acea21d7434846fa73d98c6f21
JachinYin <1106025235@qq.com>
2017-12-18 20:30:34 +0800

    水仙花数

--- num.py
+++ num.py
@@ -34,43 +34,76 @@ def waNum2(mi):
 
 
 def waNum3(mi):
-
-    for number in xrange(10**(mi-1),10**mi):
-        sum = 0
-        res = []
+    temp = []
+    num = 10 ** (mi - 1)
+    while num < 10**mi:
         d = [0 for _ in range(10)]
-        for i in str(number):
+        for i in str(num):
             d[int(i)] += 1
         d.pop(0)
-        res.append(d)
-    res = list(set(res))
+        if d not in temp:
+            temp.append(d)
+        num += 1
+        if num % (10**(mi-1)) == 0:
+            print num
 
-    for i in range(len(res)):
-        temp = res[i]
-
-
-    for number in res:
+    for number in temp:
+        fire = 0
         for i in xrange(9):
-            if d[8-i] != 0:
-                sum += d[8-i]*((9-i)**mi)
-            if sum > number:
-                break
+            fire += number[i] * (i+1)**mi
+        if len(str(fire)) == mi:
+            d = [0 for _ in range(10)]
+            for i in str(fire):
+                d[int(i)] += 1
+            d.pop(0)
+            if number == d:
+                print fire
 
-        if sum == number:
-            print number
+def waNum4(mi):
+    mi_res = [i**mi for i in range(10)]
+    #print mi_res
+    for i in range(mi):
+        for num in mi_res:
+            pass
+            #print i
+
+
+
+def waNum5(mi):
+    temp = []
+    num = 10 ** (mi - 1)
+    while num < 10**mi:
+        d = [0 for _ in range(10)]
+        for i in str(num):
+            d[int(i)] += 1
+        d.pop(0)
+        temp.append(d)
+        num += 1
+
+    res = []
+    for i in temp:
+        if i not in res:
+            res.append(i)
+
+    print len(temp),len(res)
+
+
+#waNum5(5)
+
+
 
 begin = datetime.now()
-#waNum2(4)
+#waNum2(5)
 end = datetime.now()
 print '2:用时%sS' %(str((end-begin).total_seconds()))
 
 begin = datetime.now()
 #waNum3(6)
+#waNum4(21)
 end = datetime.now()
 print '3:用时%sS' %(str((end-begin).total_seconds()))
 
 
-a = [[0,1],[1,0],[0,1]]
-if a[0]== a[2]:
-    del a[2]
-    print a
+
+import pprint
+root = [[[0,1,2],[0,1],2],[[0,1,2],[0,1]],2]

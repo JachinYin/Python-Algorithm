3cc71191d92636864edde5b39453180ec44e578d
JachinYin <1106025235@qq.com>
2017-12-20 21:38:08 +0800

    棋盘覆盖

--- 棋盘覆盖/Board.py
+++ 棋盘覆盖/Board.py
@@ -4,43 +4,49 @@ Data: 2017- 12 - 20
 Author: Jachin
 This is a scrpit for ...
 '''
-t = 0
 
-Size = 4
-b = [[0 for _ in xrange(Size)] for _ in xrange(Size)]
-def board(size,tx,ty,dx,dy):
-    global t
+def board(size, tx, ty, dx, dy):
+    global t1, b
     if size == 1: return
-    t = t + 1
-    s = size / 2
+    t1 = t1 + 1
+    t = t1
+    s = size // 2
 
-    #1
-    if dx < tx+s and dy < ty + s:
-        board(s, tx,ty,dx,dy)
+    # 1
+    if dx < tx + s and dy < ty + s:
+        board(s, tx, ty, dx, dy)
     else:
-        b[dx+s-1][dy+s-1] = t
-        board(s,tx,ty,dx+s-1,dy+s-1)
+        b[tx + s - 1][ty + s - 1] = t
+        board(s, tx, ty, tx + s - 1, ty + s - 1)
 
-    #2
-    if dx < tx+s and dy >= ty + s:
-        board(s, tx,ty+s,dx,dy)
+    # 2
+    if dx < tx + s and dy >= ty + s:
+        board(s, tx, ty + s, dx, dy)
     else:
-        b[dx+s-1][dy+s] = t
-        board(s,tx,ty+s,dx+s-1,dy+s)
+        b[tx + s - 1][ty + s] = t
+        board(s, tx, ty + s, tx + s - 1, ty + s)
 
-    #3
-    if dx >= tx+s and dy < ty + s:
-        board(s, tx+s,ty,dx,dy)
+    # 3
+    if dx >= tx + s and dy < ty + s:
+        board(s, tx + s, ty, dx, dy)
     else:
-        b[dx+s][dy+s-1] = t
-        board(s,tx+s,ty,dx+s,dy+s-1)
+        b[tx + s][ty + s - 1] = t
+        board(s, tx + s, ty, tx + s, ty + s - 1)
 
-    if dx >= tx+s and dy >= ty + s:
-        board(s, tx+s,ty+s,dx,dy)
+    # 4
+    if dx >= tx + s and dy >= ty + s:
+        board(s, tx + s, ty + s, dx, dy)
     else:
-        b[dx+s][dy+s] = t
-        board(s,tx+s,ty+s,dx+s,dy+s)
+        b[tx + s][ty + s] = t
+        board(s, tx + s, ty + s, tx + s, ty + s)
 
-board(Size,0,0,0,0)
+
+t1 = 0
+Size = 16
+
+b = [[0 for _ in xrange(Size)] for _ in xrange(Size)]
+
+
+board(Size, 0, 0, 0, 2)
 for i in b:
     print i

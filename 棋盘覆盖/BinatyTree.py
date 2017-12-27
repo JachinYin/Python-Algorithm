57ec9c0c306d0ddf901dcc0306bcc0f82be556cf
JachinYin <1106025235@qq.com>
2017-12-27 23:37:03 +0800

    二叉树

---
+++ 棋盘覆盖/BinatyTree.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+'''
+Data: 2017- 12 - 20
+Author: Jachin
+二叉树
+'''
+
+class BinTree:
+    def __init__(self):
+        pass
+
+    def build(self):
+        pass
+
+    def lChild(self):
+        pass
+
+    def rChild(self):
+        pass
+
+

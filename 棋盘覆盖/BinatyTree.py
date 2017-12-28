a64fadaf3fa6868532efd72f298b28cfad5cecd5
JachinYin <1106025235@qq.com>
2017-12-28 23:38:43 +0800

    构造二叉树

--- 棋盘覆盖/BinatyTree.py
+++ 棋盘覆盖/BinatyTree.py
@@ -4,10 +4,15 @@ Data: 2017- 12 - 20
 Author: Jachin
 二叉树
 '''
+from math import sqrt
 
 class BinTree:
     def __init__(self):
         pass
+        self.leng = 1
+        self.root = [0]
+        self.hight = 1
+
 
     def build(self):
         pass
@@ -18,4 +23,17 @@ class BinTree:
     def rChild(self):
         pass
 
+    def insert(self,num):
+        #TODO insert chilldren
+        if self.leng == 1 or self.leng == 2:
+            self.root.append([num])
+            self.leng += 1
+        self.hight = sqrt(self.leng+1)
 
+t = BinTree()
+t.insert(2)
+t.insert(7)
+t.insert(8)
+print t.hight
+print t.leng
+print t.root

90d72255345670540a8659b443b9d72450862c72
JachinYin <1106025235@qq.com>
2017-12-30 23:20:56 +0800

    二叉树

--- 棋盘覆盖/BinatyTree.py
+++ 棋盘覆盖/BinatyTree.py
@@ -6,36 +6,48 @@ Author: Jachin
 '''
 from math import sqrt
 
+class Node():
+    '''节点类'''
+    def __init__(self,elem = -1,lchild = None,rchild = None):
+        self.elem = elem
+        self.lchild = lchild
+        self.rchild = rchild
+
 class BinTree:
     def __init__(self):
+        #TODO build initial tree
+        self.root = Node()
+        self.myQueue = []
+
+    def add(self,elem):
+        #TODO add Node to a tree
         pass
-        self.leng = 1
-        self.root = [0]
-        self.hight = 1
+        node = Node(elem)
+        if self.root.elem == -1:  # 如果树是空的，则对根节点赋值
+            self.root = node
+            self.myQueue.append(self.root)
+        else:
+            treeNode = self.myQueue[0]  # 此结点的子树还没有齐。
+            if treeNode.lchild == None:
+                treeNode.lchild = node
+                self.myQueue.append(treeNode.lchild)
+            else:
+                treeNode.rchild = node
+                self.myQueue.append(treeNode.rchild)
+                self.myQueue.pop(0)  # 如果该结点存在右子树，将此结点丢弃。
 
-
-    def build(self):
-        pass
-
-
-    def lChild(self):
-        pass
-
-    def rChild(self):
-        pass
-
-    def insert(self,num):
-        #TODO insert chilldren
-        if self.leng == 1 or self.leng == 2:
-            self.root.append([num])
-            self.leng += 1
-        self.hight = sqrt(self.leng+1)
+    def front_digui(self, root):
+        """利用递归实现树的先序遍历"""
+        if root is None:
+            return
+        print root.elem,
+        self.front_digui(root.lchild)
+        self.front_digui(root.rchild)
 
 t = BinTree()
-t.insert(2)
-t.insert(7)
-t.insert(8)
-print t.hight
-print t.leng
-print t.root
+t.add(3)
+t.add(2)
+t.add(5)
+t.add(7)
+t.front_digui(t.root)
 
